2fe21f461c780b27ae1e3c62f6d2065cfff87a49
cmlozanos <carlosmlozanos@gmail.com>
2020-11-19 12:23:10 +0100

    init commit

---
+++ src/test/java/com/example/katafizzbuzzjava/KataFizzBuzzJavaApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example.katafizzbuzzjava;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class KataFizzBuzzJavaApplicationTest {
+	@Test
+	void testcase1() throws Exception {
+		Assertions.assertTrue(true);
+	}
+}
